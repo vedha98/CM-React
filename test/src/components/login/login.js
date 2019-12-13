a335701fd346c4b0c526ed1f862e6c0bbe9d5753
vedha98 <vetha.gnanam98@gmail.com>
2019-12-13 23:53:58 +0530

    added component

---
+++ test/src/components/login/login.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './login.css'
+
+class login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
+            <div>login</div>
+        );
+    }
+}
+
+export default login;
