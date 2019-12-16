3335110490b45bc4d86a6e1212fcb0a513b7d7b2
vedha98 <vetha.gnanam98@gmail.com>
2019-12-16 11:10:18 +0530

    added backend

---
+++ test/backend/models/user.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const db = require("../config/database");
+const user = db.define('data',
+{
+    
+name:{
+    type:Sequelize.STRING
+},
+desc:{
+    type:Sequelize.STRING
+},
+salary:{
+    type:Sequelize.STRING
+},
+createdAt:{
+    type:Sequelize.DATE
+},
+updatedAt:{
+    type:Sequelize.DATE
+}
+}
+)
+module.exports = user;
