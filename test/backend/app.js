fb61bb30a50b210a1debd6d161abe2abfe39e03d
vedha98 <vetha.gnanam98@gmail.com>
2019-12-16 13:06:07 +0530

    backed and cors

--- test/backend/app.js
+++ test/backend/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const cors = require("cors");
 const app = express();
 app.get("/",(req,res)=>{
     res.json({msg:"success"});
 })
-
+app.use(cors());
 app.use(bodyparser.json());
 const db = require('./config/database');
 db.authenticate().then(console.log("true")).catch(err=>{console.log(err)});
+
+
 app.use('/user',require('./routes/users'))
 
 
--- test/src/components/login/login.js
+++ test/src/components/login/login.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './login.css'
-
+const axios = require('axios');
 class login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {  };
     }
     render() {
+    
         return (
             <div>login</div>
         );
+    }
+    componentDidMount(){
+        axios.get('http://localhost:8000/user')
+  .then(function (response) {
+    console.log(response.data);
+  })
     }
 }
 
