0621030af845596bdfc1eefd024db1ba3c359663
vedha98 <vetha.gnanam98@gmail.com>
2019-12-18 11:28:50 +0530

    added home route

--- test/src/App.js
+++ test/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import Login from './components/login/login'
+import Login from './components/login/login';
+import Home from './components/home/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 
 } from "react-router-dom";
 
 function App() {
   return (
+    <div className="app">
           <Router>
       
         
@@ -18,11 +22,14 @@ function App() {
         <Switch>
           
           
-          <Route exact path="/admin" component={Login}/>
+          <Route exact path="/user/login" component={Login}/>
+          <Route exact path="/home" component={Home}/>
+          <Route  path="*"  render={() => (<Redirect to="/home" />)}/>
+          
         </Switch>
      
     </Router>
-        
+    </div>
         
  
   );
--- test/src/components/login/login.js
+++ test/src/components/login/login.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import './login.css'
+import { Button, Form, Col,Row, Container } from 'react-bootstrap';
+import './login.css';
+
 const axios = require('axios');
+
 class login extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {  };
+        this.state = {};
+
     }
     render() {
-    
+
         return (
-            <div>
-            
-            
-            </div>
+<Container><Row><Col md={{ span: 6, offset: 3 }}>
+    <Container>
+            <Form>
+                <Form.Group as={Row} controlId="formBasicName">
+                    <Form.Label column sm="2">
+                        Email
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control  placeholder="Enter your User ID" />
+                    </Col>
+                </Form.Group>
+                <Form.Group as={Row} controlId="formBasicPassword">
+                    <Form.Label column sm="2">
+                        Password
+                    </Form.Label>
+                    <Col sm="10">
+                        <Form.Control  placeholder="Enter your Password" />
+                    </Col>
+                </Form.Group>              
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            </Container>
+            </Col>
+            </Row>
+            </Container>
         );
     }
-    componentDidMount(){
+    componentDidMount() {
         axios.get('http://localhost:8000/user')
-  .then(function (response) {
-    console.log(response.data);
-  })
+            .then(function (response) {
+                console.log(response.data);
+            })
     }
 }
 
